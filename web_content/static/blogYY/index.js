6c8507595a6d7b2a548b9df2cfc778e9bbb917ad
shifvb <shifvb@gmail.com>
2020-04-25 14:05:29 +0800

    update: remove data-raw attribute after quill interpreted article content

--- web_content/static/blogYY/index.js
+++ web_content/static/blogYY/index.js
@@ -2,6 +2,7 @@ $(function(){
     // 使用quill解释文章内容
     document.querySelectorAll(".article_content").forEach(function(x) {
         new Quill(x).setContents(JSON.parse(x.dataset.raw));
+        x.removeAttribute("data-raw");
     });
 
     // 删除article按钮鼠标单击事件绑定
