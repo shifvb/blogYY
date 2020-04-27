82eac463326ac299fdd99a36e0bbb2b4b2644574
shifvb <shifvb@gmail.com>
2020-04-27 14:16:42 +0800

    bug fix: login

--- web_content/src/blogYY/mod_user/models.py
+++ web_content/src/blogYY/mod_user/models.py
@@ -4,39 +4,12 @@ from flask import g
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
-from flask_login import UserMixin
-import json
-import uuid
-
-# define profile.json constant, the file is used to
-# save user name and password_hash
-PROFILE_FILE = "profiles.json"
 
 
 class User(UserMixin):
     def __init__(self, username):
         self.username = username
         self.id = self.get_id()
-
-    @property
-    def password(self):
-        return AttributeError("password is not a readable attribute")
-
-    @password.setter
-    def password(self, password):
-        """
-        save user name, id and password hash to json file
-        :param password:
-        :return:
-        """
-        self.password_hash = generate_password_hash(password) # todo: remove self
-        with open(PROFILE_FILE, 'w+') as f:
-            try:
-                profiles = json.load(f)
-            except ValueError:
-                profiles = {}
-            profiles[self.username] = [self.password_hash, self.id]
-            f.write(json.dumps(profiles))
 
     def verify_password(self, password):
         password_hash = self.get_password_hash()
@@ -54,15 +27,16 @@ class User(UserMixin):
             return None.
         """
         try:
-            with open(PROFILE_FILE) as f:
-                user_profiles = json.load(f)
-                user_info = user_profiles.get(self.username, None)
-                if user_info is not None:
-                    return user_info[0]
+            _cursor = g.blogYY_conn.cursor()
+            _cursor.execute("""SELECT `password_hash` FROM `user` WHERE `username`=?;""", (self.username,))
+            _user_info_tuple = _cursor.fetchone()
+            if _user_info_tuple is not None:
+                return _user_info_tuple[0]
         except IOError:
             return None
         except ValueError:
             return None
+        return None
 
     def get_id(self):
         """
@@ -93,11 +67,11 @@ class User(UserMixin):
         if not user_id:
             return None
         try:
-            with open(PROFILE_FILE) as f:
-                user_profiles = json.load(f)
-                for user_name, profile in user_profiles.items():
-                    if profile[1] == user_id:
-                        return User(user_name)
+            _cursor = g.blogYY_conn.cursor()
+            _cursor.execute("""SELECT `username` FROM `user` WHERE `uuid`=?;""", (user_id,))
+            _user_info_tuple = _cursor.fetchone()
+            if _user_info_tuple is not None:
+                return User(_user_info_tuple[0])
         except:
             return None
         return None
